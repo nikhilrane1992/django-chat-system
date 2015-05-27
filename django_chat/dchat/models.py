1dfe1016ed4bd7ec1e9f350ad3db6fdecba51168
Nikhil Rane <nikhilrane1992@hmail.com>
2015-05-27 11:16:28 +0530

    setup static files, create models, and create html template

--- django_chat/dchat/models.py
+++ django_chat/dchat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class messages(models.Model):
+    '''Representation of a generic chat room'''
+    # belongs_to_type = models.CharField(max_length=100, blank=True, null=True)
+    # belongs_to_id = models.IntegerField(blank=True, null=True)
+    created = models.DateTimeField()
+    comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey('User')
+    # objects = RoomManager() # custom manager
+
+    def return_user(self):
+    	return self.user
+
+
+
--- django_chat/dchat/views.py
+++ django_chat/dchat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render_to_response
+
 
 # Create your views here.
+
+
+## view for display home page
