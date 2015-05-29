815d84e6dd6f61cb0572e3d638305f2d2439a741
rahool <rahool@contrailtech.com>
2015-05-29 15:00:40 +0530

    displayed 3 chat boxes

--- django_chat/static/angular/expertChatApp.js
+++ django_chat/static/angular/expertChatApp.js
@@ -1,123 +1,132 @@
 (function(){
-    var chatApp = angular.module('expertChatApp',[]);
-    chatApp.controller('expertChatCtrl', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout){
+	var chatApp = angular.module('expertChatApp',[]);
+	chatApp.controller('expertChatCtrl', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout){
 
-        $scope.messageToSend = "";
-        $scope.chat_room_id = "";
-        $scope.last_received = "";
-        $scope.loginUser = "";
-        $scope.init = function () {
-            $log.debug("Hello guest");
-            getChatIdFromServer();
-        };
-        $timeout($scope.init);
+		$(window).unload(function(){chat_leave()});
 
-        var getChatIdFromServer = function() {
-            $http.get('/chat/room/id/').then(function (response) {
-                $log.debug(response.data);
-                $scope.chat_room_id = response.data.chat_id;
-                $scope.loginUser = response.data.user_name;
-                $log.debug($scope.chat_room_id);
-                $log.debug(parseInt($scope.chat_room_id));
-                chat_join();
+		$scope.messageToSend = "";
+		$scope.chat_room_id = "";
+		$scope.last_received = "";
+		$scope.loginUser = "";
+		$scope.init = function () {
+			$log.debug("Hello guest");
+			getChatIdFromServer();
+		};
+		$timeout($scope.init);
 
-            });
-        };
+		var getChatIdFromServer = function() {
+			$http.get('/chat/room/id/').then(function (response) {
+				$log.debug(response.data);
+				$scope.chat_room_id = response.data.chat_id;
+				$scope.loginUser = response.data.user_name;
+				$log.debug($scope.chat_room_id);
+				$log.debug(parseInt($scope.chat_room_id));
+				chat_join();
 
-        $scope.sendMessage = function(messageToSend) {
-            // $log.debug(messageToSend);
-            $scope.chat_room_id = parseInt($scope.chat_room_id);
-            if(messageToSend.length != 0){
-                $http.post('/chat/send/',{message:messageToSend,chat_room_id:$scope.chat_room_id}).then(function (response) {
-                    $log.debug(response.data);
-                });
-            }
-        };
+			});
+		};
 
-        $scope.get_messages = function() {
-            $scope.last_received = parseInt($scope.last_received);
-            $http.post('/chat/receive/',{id:$scope.chat_room_id, offset: $scope.last_received}).then(function (response) {
-                $log.debug(response.data);
+		$scope.sendMessage = function(messageToSend) {
+			// $log.debug(messageToSend);
+			$scope.chat_room_id = parseInt($scope.chat_room_id);
+			if(messageToSend.length != 0){
+				$http.post('/chat/send/',{message:messageToSend,chat_room_id:$scope.chat_room_id}).then(function (response) {
+					$log.debug(response.data);
+				});
+			}
+		};
 
-                angular.forEach(response.data, function(obj) {
-                    if (obj.type == 's')
-                        $('.msg_container_base').append('<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_sent"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">Timothy • 51 min</time></div></div><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div></div>');
-                    else if (obj.type == 'm'){
-                        if(obj.author == $scope.loginUser){
-                            $('.msg_container_base').append('<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_sent"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">'+obj.author+' • 51 min</time></div></div><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div></div>');
-                        }else{
-                            $('.msg_container_base').append('<div class="row msg_container base_receive"><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_receive"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">'+obj.author+' • 51 min</time></div></div></div>');
-                        }
-                    }
-                    else if (obj.type == 'j')
-                        $('.msg_container_base').append('<div class="join">'+obj.author+' has joined</div>');
-                    else if (obj.type == 'l')
-                        $('.msg_container_base').append('<div class="leave">'+obj.author+' has left</div>');
+		$scope.get_messages = function() {
+			$scope.last_received = parseInt($scope.last_received);
+			$http.post('/chat/receive/',{id:$scope.chat_room_id, offset: $scope.last_received}).then(function (response) {
+				$log.debug(response.data);
 
-                    $scope.last_received = obj.id;
-                    $log.debug("Last received: " + $scope.last_received);
-                });
-            });
-            $timeout(function(){$scope.get_messages();}, 5000);
-        }
+				angular.forEach(response.data, function(obj) {
+					if (obj.type == 's')
+						$('.msg_container_base').append('<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_sent"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">Timothy • 51 min</time></div></div><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div></div>');
+					else if (obj.type == 'm'){
+						if(obj.author == $scope.loginUser){
+							$('.msg_container_base').append('<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_sent"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">'+obj.author+' • 51 min</time></div></div><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div></div>');
+						}else{
+							$('.msg_container_base').append('<div class="row msg_container base_receive"><div class="col-md-2 col-xs-2 avatar"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive "></div><div class="col-md-10 col-xs-10 chat-window-body"><div class="messages msg_receive"><p>'+ replace_emoticons(obj.message) +'</p><time datetime="2009-11-13T20:00">'+obj.author+' • 51 min</time></div></div></div>');
+						}
+					}
+					else if (obj.type == 'j')
+						$('.msg_container_base').append('<div class="join">'+obj.author+' has joined</div>');
+					else if (obj.type == 'l')
+						$('.msg_container_base').append('<div class="leave">'+obj.author+' has left</div>');
 
-        var sync_messages = function() {
+					$scope.last_received = obj.id;
+					$log.debug("Last received: " + $scope.last_received);
+				});
+			});
+			$timeout(function(){$scope.get_messages();}, 5000);
+		}
 
-            $scope.chat_room_id = parseInt($scope.chat_room_id);
-            $http.post('/chat/sync/',{id:$scope.chat_room_id}).then(function (response) {
-                $log.debug(response.data);
-                $scope.last_received = response.data.last_message_id;
+		var sync_messages = function() {
 
-            });
+			$scope.chat_room_id = parseInt($scope.chat_room_id);
+			$http.post('/chat/sync/',{id:$scope.chat_room_id}).then(function (response) {
+				$log.debug(response.data);
+				$scope.last_received = response.data.last_message_id;
 
-            $timeout(function(){$scope.get_messages();}, 5000);
-        };
+			});
 
-
-        // emoticons
-        var emoticons = {
-            '>:D' : 'emoticon_evilgrin.png',
-            ':D' : 'emoticon_grin.png',
-            '=D' : 'emoticon_happy.png',
-            ':\\)' : 'emoticon_smile.png',
-            ':O' : 'emoticon_surprised.png',
-            ':P' : 'emoticon_tongue.png',
-            ':\\(' : 'emoticon_unhappy.png',
-            ':3' : 'emoticon_waii.png',
-            ';\\)' : 'emoticon_wink.png',
-            '\\(ball\\)' : 'sport_soccer.png'
-        }
-
-        /**
-         * Regular expression maddness!!!
-         * Replace the above strings for their img counterpart
-         */
-        function replace_emoticons(text) {
-            $.each(emoticons, function(char, img) {
-                re = new RegExp(char,'g');
-                // replace the following at will
-                text = text.replace(re, '<img src="/media/img/silk/'+img+'" />');
-            });
-            return text;
-        }
-
-        function chat_join() {
-            $http.post('/chat/join/',{chat_room_id:$scope.chat_room_id}).then(function (response) {
-                sync_messages();
-            });
-        }
-
-        function chat_leave() {
-            $http.post('/chat/leave/',{chat_room_id:$scope.chat_room_id}).then(function (response) {
-
-            });
-        }
-
-        $(window).unload(function(){chat_leave()});
+			$timeout(function(){$scope.get_messages();}, 5000);
+		};
 
 
+		// emoticons
+		var emoticons = {
+			'>:D' : 'emoticon_evilgrin.png',
+			':D' : 'emoticon_grin.png',
+			'=D' : 'emoticon_happy.png',
+			':\\)' : 'emoticon_smile.png',
+			':O' : 'emoticon_surprised.png',
+			':P' : 'emoticon_tongue.png',
+			':\\(' : 'emoticon_unhappy.png',
+			':3' : 'emoticon_waii.png',
+			';\\)' : 'emoticon_wink.png',
+			'\\(ball\\)' : 'sport_soccer.png'
+		}
 
-    }]); //controller ends
+		/**
+		 * Regular expression maddness!!!
+		 * Replace the above strings for their img counterpart
+		 */
+		function replace_emoticons(text) {
+			$.each(emoticons, function(char, img) {
+				re = new RegExp(char,'g');
+				// replace the following at will
+				text = text.replace(re, '<img src="/media/img/silk/'+img+'" />');
+			});
+			return text;
+		}
+
+		function chat_join() {
+			$http.post('/chat/join/',{chat_room_id:$scope.chat_room_id}).then(function (response) {
+				sync_messages();
+			});
+		}
+
+		function chat_leave() {
+			$http.post('/chat/leave/',{chat_room_id:$scope.chat_room_id}).then(function (response) {
+
+			});
+		}
+
+		for(var i = 0; i < 2; i++ ){
+			var size = $( ".chat-window:last-child" ).css("margin-left");
+			size_total = parseInt(size) + 400;
+			// alert(size_total);
+			var clone = $( ".chat-window" ).clone().appendTo( ".container" );
+			clone.css("margin-left", size_total);
+			clone.addClass("new_"+i);
+			clone.removeAttr('id');
+		}
+
+
+	}]); //controller ends
 
 
 
